c97f711d70cec9658e9b215647a07b1471bba5ff
MateusOliveira <mateuschagasoliveira@gmail.com>
2021-08-02 09:57:54 -0300

    Desfaio .Net

---
+++ WebApi/Startup.cs
@@ -0,0 +1,70 @@
+using Application;
+using Application.Interfaces;
+using Infrastructure.Identity;
+using Infrastructure.Identity.Contexts;
+using Infrastructure.Persistence;
+using Infrastructure.Shared;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
+using WebApi.Extensions;
+using WebApi.Services;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public IConfiguration _config { get; }
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationLayer();
+            services.AddIdentityInfrastructure(_config);
+            services.AddPersistenceInfrastructure(_config);
+            services.AddSharedInfrastructure(_config);
+            services.AddSwaggerExtension();
+            services.AddControllers();
+            services.AddApiVersioningExtension();
+            services.AddHealthChecks();
+            services.AddScoped<IAuthenticatedUserService, AuthenticatedUserService>();            
+
+            services.AddDbContext<IdentityContext>(options =>
+            {
+                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseSwaggerExtension();
+            app.UseErrorHandlingMiddleware();
+            app.UseHealthChecks("/health");
+
+            app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapControllers();
+             });
+        }
+    }
+}
